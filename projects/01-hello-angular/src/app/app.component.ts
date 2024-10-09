f3941739f7d14ec3d3587785469d75b613f3b552
AlejandraSG0103 <sgomezalejandra@gmail.com>
2024-10-08 20:19:40 -0600

    Exercise 1 - rest of the files

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,17 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+
+    <ol>
+      <li>Interestelar</li>
+      <li>Devil wears prada</li>
+      <li>The time travelers wife</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+  ol{
+    list-style-type: upper-roman;
+  }
+  `,
 })
 export class AppComponent {}
